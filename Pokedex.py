b5f48418f8f09872b6803d933649b76f9afddeae
winterobin <214276074+winterobin@users.noreply.github.com>
2025-06-06 10:37:56 +0200

    Confront added

--- Pokedex.py
+++ Pokedex.py
@@ -16,95 +16,133 @@ def _():
 
 @app.cell
 def _(pd, requests, yaml):
-    #df = pd.read_csv("data/Pokemon.csv")
-
     url = "https://raw.githubusercontent.com/pokemondb/database/refs/heads/master/data/pokemon-forms.yaml"
 
     response = requests.get(url)
-    response.raise_for_status()  # solleva un errore se la richiesta fallisce
+    response.raise_for_status()
 
     yaml_text = response.text
     yaml_text = yaml_text.replace("type2: -", 'type2: ""')
 
-    # Ora puoi fare il parsing in sicurezza
+    # parsing
     data = yaml.safe_load(yaml_text)
-
-    # Verifica i primi elementi
-    #print(data)
 
     df = pd.DataFrame(data).T
     stats_df = df["stats"].apply(pd.Series)
     df = df.join(stats_df)
     df.drop(columns=['stats'], inplace=True)
+
+    df = df.reset_index()
     return (df,)
 
 
 @app.cell
 def _(df, mo):
-    # Crea il menu a tendina con i nomi dei Pokémon
-    dropdown1 = mo.ui.dropdown.from_series(df["type1"], label="Scegli il Tipo 1 del Pokémon", searchable=True)
-    dropdown2 = mo.ui.dropdown.from_series(df["type2"], label="Scegli il Tipo 2 del Pokémon", searchable=True)
+    # Create the menu
+    dropdownname = mo.ui.dropdown.from_series(df["index"], label="Choose the first Pokémon name", searchable=True)
+    dropdownname2 = mo.ui.dropdown.from_series(df["index"], label="Choose the second Pokémon name", searchable=True)
+    dropdown1 = mo.ui.dropdown.from_series(df["type1"], label="Choose the first Type", searchable=True)
+    dropdown2 = mo.ui.dropdown.from_series(df["type2"], label="Choose the second Type", searchable=True)
 
-    # Visualizza il menu
-    dropdown1, dropdown2
-    return dropdown1, dropdown2
+    # Show the menu
+    mo.md(f"""Here's a quote""")
+    dropdownname, dropdownname2, dropdown1, dropdown2
+    return dropdown1, dropdown2, dropdownname, dropdownname2
 
 
 @app.cell
-def _(df, dropdown1, dropdown2):
-    # Filtra il DataFrame in base alla selezione
-    Tipo1 = dropdown1.value
-    Tipo2 = dropdown2.value
-    if not Tipo2:
-        dati_pokemon = df[df["type1"] == Tipo1]
-    else:
-        dati_pokemon = df[(df["type1"] == Tipo1) & (df["type2"] == Tipo2)]
-    return (dati_pokemon,)
+def _(df, dropdown1, dropdown2, dropdownname, dropdownname2):
+    # DataFrame filtering
+    Name = dropdownname.value
+    Name2 = dropdownname2.value
+    Type1 = dropdown1.value
+    Type2 = dropdown2.value
+    if Name and not Name2 and not Type1 and not Type2:
+        pokemon_data = df[df["index"] == Name]
+    if Name and Name2 and not Type1 and not Type2:
+        pokemon_data = df[(df["index"] == Name) | (df["index"] == Name2)]
+    if not Name and Type1:
+        pokemon_data = df[df["type1"] == Type1]
+    if not Name and Type1 and Type2:
+        pokemon_data = df[(df["type1"] == Type1) & (df["type2"] == Type2)]
+    return (pokemon_data,)
 
 
 @app.cell
 def _(df, mo):
-    colonne_disponibili = df.columns.tolist()
+    available_columns = df.columns.tolist()
 
     # Widget: elenco a crocette
-    colonne_scelte = mo.ui.multiselect(
-        options=colonne_disponibili,
-        value=["pokemonid", "type1", "type2", "hp", "attack","defense","spatk","spdef","speed"],  # valori iniziali
-        label="Scegli le colonne da visualizzare:"
+    chosen_columns = mo.ui.multiselect(
+        options=available_columns,
+        value=["index", "formid", "type1", "type2", "hp", "attack","defense","spatk","spdef","speed"],  # starting values
+        label="Choose the columns to show:"
     )
 
-    colonne_scelte
-    return (colonne_scelte,)
+    chosen_columns
+    return (chosen_columns,)
 
 
 @app.cell
-def _(colonne_scelte, dati_pokemon):
-    dati_pokemon[colonne_scelte.value]
+def _(chosen_columns, pokemon_data):
+    pokemon_data[chosen_columns.value]
     return
 
 
 @app.cell
-def _(alt, dati_pokemon):
-    # Trasforma in long format
+def _(alt, mo, pokemon_data):
+    # Graphs
+
     stats = ["hp", "attack", "defense", "spatk", "spdef", "speed"]
-    df_stat = dati_pokemon[["pokemonid"] + stats].melt(
-        id_vars="pokemonid", var_name="Stat", value_name="Value"
+    df_stat = pokemon_data[["index"] + stats].melt(
+        id_vars="index", var_name="Stat", value_name="Value"
     )
 
-    # Altair chart
-    chart = (
-        alt.Chart(df_stat)
-        .mark_point()
-        .encode(
-            x=alt.X("Stat:N", sort=stats),
-            y="Value:Q",
-            color="Stat:N",
-            tooltip=["pokemonid:N", "Stat:N", "Value:Q"]
+    if len(pokemon_data) == 1:
+        chart = (
+            alt.Chart(df_stat)
+            .mark_bar()
+            .encode(
+                y=alt.Y("Stat:N", sort=stats),
+                x=alt.X("Value:Q").scale(domain=(0, 255)),
+                color=alt.Color("Stat:N", title="Pokémon", sort=["hp", "attack","defense", "spatk","spdef", "speed"]),
+                tooltip=["index:N", "Stat:N", "Value:Q"]
+            )
+            #.properties(title=f"Statistiche di {pokemon}")
         )
-        #.properties(title=f"Statistiche di {pokemon}")
-    )
 
-    chart
+    if len(pokemon_data) == 2:
+        df_stat["Stat_Pokemon"] = df_stat["Stat"] + " - " + df_stat["index"]
+        ordered_stats = df_stat["Stat_Pokemon"].tolist()
+    
+        chart = (
+            alt.Chart(df_stat)
+            .mark_bar()
+            .encode(
+                y=alt.Y("Stat_Pokemon:N", sort=ordered_stats, title=""),
+                x=alt.X("Value:Q", scale=alt.Scale(domain=(0, 255))),
+                color=alt.Color("Stat:N", title="Pokémon", sort=["hp", "attack","defense", "spatk","spdef", "speed"]),
+                tooltip=["Stat:N", "index:N", "Value:Q"]
+            )
+        )
+
+    if len(pokemon_data) >= 3:
+        # Altair chart
+        chart = (
+            alt.Chart(df_stat)
+            .mark_point()
+            .encode(
+                x=alt.X("Stat:N", sort=stats),
+                y=alt.Y("Value:Q").scale(domain=(0, 255)),
+                color=alt.Color("Stat:N", title="Pokémon", sort=["hp", "attack","defense", "spatk","spdef", "speed"]),
+                tooltip=["index:N", "Stat:N", "Value:Q"]
+            )
+            #.properties(title=f"Statistiche di {pokemon}")
+        )
+
+    mo.ui.altair_chart(chart)
+
+
     return
 
 
